240c8278684760955732de2ee10d3a4f3c821edc
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2025-01-05 05:23:45 +0900

    Refactor

--- docs/__corpus_statistics.js
+++ docs/__corpus_statistics.js
@@ -2,9 +2,8 @@ import { EARTHLING_LIST, EARTHLING_WORDS, isEarthlingWord } from './earthling.js
 import { tokenize } from './get_singly_annotated_line.js';
 import { corpus_new_to_old } from "./search.js";
 import { queryLemma } from "./query_lemma.js";
-window.gen_stat = function () {
-    const occurrence_map = new Map();
-    const highlightable = [];
+function categorize() {
+    const highlightable_occurrence_map = new Map();
     const non_highlightable = [];
     const earthling = [];
     const t0 = performance.now();
@@ -16,8 +15,7 @@ window.gen_stat = function () {
                 const query_res = queryLemma(tok.content, true);
                 if (query_res.kind === "ok") {
                     const lemma = query_res.words.map(w => `(${w.語}|${w.品詞})`).join("");
-                    occurrence_map.set(lemma, (occurrence_map.get(lemma) || 0) + 1);
-                    highlightable.push(tok.content);
+                    highlightable_occurrence_map.set(lemma, (highlightable_occurrence_map.get(lemma) || 0) + 1);
                 }
                 else if (isEarthlingWord(tok.content)) {
                     earthling.push(tok.content);
@@ -29,29 +27,35 @@ window.gen_stat = function () {
         }
     }
     const t1 = performance.now();
-    console.log(`In count_highlightable():\nOuter loop required ${(t1 - t0).toFixed(2)} milliseconds.`);
-    const countedOccurrences = [...occurrence_map.entries()];
-    const sum = countedOccurrences.reduce((acc, [_k, v]) => acc + v, 0);
-    document.getElementById("total-count").textContent = sum.toString();
-    countedOccurrences.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
-    document.getElementById("output-freq-ranking").value = countedOccurrences.map(([k, v]) => `${v}\t${k}`).join("\n");
-    const highlightable_uniq = new Set(highlightable);
+    console.log(`Analyzing all the words in the corpus required ${(t1 - t0).toFixed(2)} milliseconds.`);
+    return { highlightable_occurrence_map, non_highlightable, earthling };
+}
+window.gen_stat = function () {
+    const { highlightable_occurrence_map, non_highlightable, earthling } = categorize();
+    // handle highlightable
+    const highlightable_uniq_count = highlightable_occurrence_map.size;
+    const highlightable_occurrence_arr = [...highlightable_occurrence_map.entries()];
+    const highlightable_non_uniq_count = highlightable_occurrence_arr.reduce((acc, [_k, v]) => acc + v, 0);
+    document.getElementById("total-count").textContent = highlightable_non_uniq_count.toString();
+    highlightable_occurrence_arr.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
+    document.getElementById("output-freq-ranking").value = highlightable_occurrence_arr.map(([k, v]) => `${v}\t${k}`).join("\n");
+    // handle non-highlightable
     const non_highlightable_uniq = new Set(non_highlightable);
+    const non_highlightable_occurrence_arr = [...non_highlightable.reduce((count, cur) => (count.set(cur, (count.get(cur) || 0) + 1), count), new Map())];
+    non_highlightable_occurrence_arr.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
     const earthling_uniq = new Set(earthling);
-    const counted = [...non_highlightable.reduce((count, cur) => (count.set(cur, (count.get(cur) || 0) + 1), count), new Map())];
-    counted.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
-    const non_uniq_total = highlightable.length + non_highlightable.length + earthling.length;
-    const uniq_total = highlightable_uniq.size + non_highlightable_uniq.size + earthling_uniq.size;
+    const non_uniq_total = highlightable_non_uniq_count + non_highlightable.length + earthling.length;
+    const uniq_total = highlightable_uniq_count + non_highlightable_uniq.size + earthling_uniq.size;
     document.getElementById("output-non-highlightable").value = `
-        highlightable (not uniq): ${highlightable.length}; ${(highlightable.length / non_uniq_total * 100).toPrecision(4)}%
+        highlightable (not uniq): ${highlightable_non_uniq_count}; ${(highlightable_non_uniq_count / non_uniq_total * 100).toPrecision(4)}%
     non-highlightable (not uniq): ${non_highlightable.length}; ${(non_highlightable.length / non_uniq_total * 100).toPrecision(4)}%
         earthling     (not uniq): ${earthling.length}; ${(earthling.length / non_uniq_total * 100).toPrecision(4)}%
     
-        highlightable (uniq): ${highlightable_uniq.size}; ${(highlightable_uniq.size / uniq_total * 100).toPrecision(4)}%
+        highlightable (uniq): ${highlightable_uniq_count}; ${(highlightable_uniq_count / uniq_total * 100).toPrecision(4)}%
     non-highlightable (uniq): ${non_highlightable_uniq.size}; ${(non_highlightable_uniq.size / uniq_total * 100).toPrecision(4)}%
         earthling     (uniq): ${earthling_uniq.size}; ${(earthling_uniq.size / uniq_total * 100).toPrecision(4)}%
         
-    top-tier non-highlightable: ${JSON.stringify(counted)}
+    top-tier non-highlightable: ${JSON.stringify(non_highlightable_occurrence_arr)}
     `;
     document.getElementById("earthling_list").textContent = JSON.stringify(EARTHLING_LIST, null, 2);
     document.getElementById("allowed_sources").innerHTML = "<ul>" + [...EARTHLING_WORDS].map(([source, words]) => `<li>${source}: ${words.join(", ")}</li>`).join("") + "</ul>";
--- ts-src/__corpus_statistics.ts
+++ ts-src/__corpus_statistics.ts
@@ -3,9 +3,8 @@ import { tokenize } from './get_singly_annotated_line.js';
 import { corpus_new_to_old } from "./search.js";
 import { queryLemma } from "./query_lemma.js";
 
-(window as any).gen_stat = function () {
+function categorize() {
     const highlightable_occurrence_map: Map<string, number> = new Map();
-    const highlightable: string[] = [];
     const non_highlightable: string[] = [];
     const earthling: string[] = [];
 
@@ -19,7 +18,6 @@ import { queryLemma } from "./query_lemma.js";
                 if (query_res.kind === "ok") {
                     const lemma = query_res.words.map(w => `(${w.語}|${w.品詞})`).join("");
                     highlightable_occurrence_map.set(lemma, (highlightable_occurrence_map.get(lemma) || 0) + 1);
-                    highlightable.push(tok.content);
                 } else if (isEarthlingWord(tok.content)) {
                     earthling.push(tok.content);
                 } else {
@@ -29,39 +27,47 @@ import { queryLemma } from "./query_lemma.js";
         }
     }
     const t1 = performance.now();
-    console.log(`In count_highlightable():\nOuter loop required ${(t1 - t0).toFixed(2)} milliseconds.`);
+    console.log(`Analyzing all the words in the corpus required ${(t1 - t0).toFixed(2)} milliseconds.`);
 
+    return { highlightable_occurrence_map, non_highlightable, earthling };
+}
+
+(window as any).gen_stat = function () {
+    const { highlightable_occurrence_map, non_highlightable, earthling } = categorize();
+
+    // handle highlightable
+    const highlightable_uniq_count = highlightable_occurrence_map.size;
     const highlightable_occurrence_arr: [string, number][] = [...highlightable_occurrence_map.entries()];
-    const sum = highlightable_occurrence_arr.reduce((acc, [_k, v]) => acc + v, 0);
-    document.getElementById("total-count")!.textContent = sum.toString();
+    const highlightable_non_uniq_count = highlightable_occurrence_arr.reduce((acc, [_k, v]) => acc + v, 0);
+    document.getElementById("total-count")!.textContent = highlightable_non_uniq_count.toString();
     highlightable_occurrence_arr.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
 
     (document.getElementById("output-freq-ranking")! as HTMLTextAreaElement).value = highlightable_occurrence_arr.map(([k, v]) => `${v}\t${k}`).join("\n");
 
-    const highlightable_uniq = new Set(highlightable);
+    // handle non-highlightable
     const non_highlightable_uniq = new Set(non_highlightable);
-    const earthling_uniq = new Set(earthling);
-
-    const counted: [string, number][] = [...non_highlightable.reduce(
+    const non_highlightable_occurrence_arr: [string, number][] = [...non_highlightable.reduce(
         (count: Map<string, number>, cur) => (count.set(cur, (count.get(cur) || 0) + 1), count),
         new Map()
     )];
 
-    counted.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
+    non_highlightable_occurrence_arr.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
 
-    const non_uniq_total = highlightable.length + non_highlightable.length + earthling.length;
-    const uniq_total = highlightable_uniq.size + non_highlightable_uniq.size + earthling_uniq.size;
+    const earthling_uniq = new Set(earthling);
+
+    const non_uniq_total = highlightable_non_uniq_count + non_highlightable.length + earthling.length;
+    const uniq_total = highlightable_uniq_count + non_highlightable_uniq.size + earthling_uniq.size;
 
     (document.getElementById("output-non-highlightable")! as HTMLTextAreaElement).value = `
-        highlightable (not uniq): ${highlightable.length}; ${(highlightable.length / non_uniq_total * 100).toPrecision(4)}%
+        highlightable (not uniq): ${highlightable_non_uniq_count}; ${(highlightable_non_uniq_count / non_uniq_total * 100).toPrecision(4)}%
     non-highlightable (not uniq): ${non_highlightable.length}; ${(non_highlightable.length / non_uniq_total * 100).toPrecision(4)}%
         earthling     (not uniq): ${earthling.length}; ${(earthling.length / non_uniq_total * 100).toPrecision(4)}%
     
-        highlightable (uniq): ${highlightable_uniq.size}; ${(highlightable_uniq.size / uniq_total * 100).toPrecision(4)}%
+        highlightable (uniq): ${highlightable_uniq_count}; ${(highlightable_uniq_count / uniq_total * 100).toPrecision(4)}%
     non-highlightable (uniq): ${non_highlightable_uniq.size}; ${(non_highlightable_uniq.size / uniq_total * 100).toPrecision(4)}%
         earthling     (uniq): ${earthling_uniq.size}; ${(earthling_uniq.size / uniq_total * 100).toPrecision(4)}%
         
-    top-tier non-highlightable: ${JSON.stringify(counted)}
+    top-tier non-highlightable: ${JSON.stringify(non_highlightable_occurrence_arr)}
     `;
 
     document.getElementById("earthling_list")!.textContent = JSON.stringify(EARTHLING_LIST, null, 2);
