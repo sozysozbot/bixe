75752a14fde45a1ae33aadc9488be349ade16a69
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2025-01-05 04:52:11 +0900

    de-indent

--- docs/__corpus_statistics.js
+++ docs/__corpus_statistics.js
@@ -3,63 +3,60 @@ import { tokenize } from './get_singly_annotated_line.js';
 import { corpus_new_to_old } from "./search.js";
 import { queryLemma } from "./query_lemma.js";
 window.gen_stat = function () {
-    {
-        const occurrence_map = new Map();
-        for (const item of corpus_new_to_old) {
-            const { pmcp: pmcp_text } = item;
-            const tokens = tokenize(pmcp_text);
-            for (const tok of tokens) {
-                if (tok.kind === "pmcp-word") {
-                    const query_res = queryLemma(tok.content, true);
-                    if (query_res.kind === "ok") {
-                        const lemma = query_res.words.map(w => `(${w.語}|${w.品詞})`).join("");
-                        occurrence_map.set(lemma, (occurrence_map.get(lemma) || 0) + 1);
-                    }
+    const occurrence_map = new Map();
+    for (const item of corpus_new_to_old) {
+        const { pmcp: pmcp_text } = item;
+        const tokens = tokenize(pmcp_text);
+        for (const tok of tokens) {
+            if (tok.kind === "pmcp-word") {
+                const query_res = queryLemma(tok.content, true);
+                if (query_res.kind === "ok") {
+                    const lemma = query_res.words.map(w => `(${w.語}|${w.品詞})`).join("");
+                    occurrence_map.set(lemma, (occurrence_map.get(lemma) || 0) + 1);
                 }
             }
         }
-        const countedOccurrences = [...occurrence_map.entries()];
-        const sum = countedOccurrences.reduce((acc, [_k, v]) => acc + v, 0);
-        document.getElementById("total-count").textContent = sum.toString();
-        countedOccurrences.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
-        document.getElementById("output-freq-ranking").value = countedOccurrences.map(([k, v]) => `${v}\t${k}`).join("\n");
     }
-    {
-        const highlightable = [];
-        const non_highlightable = [];
-        const earthling = [];
-        const t0 = performance.now();
-        for (const item of corpus_new_to_old) {
-            //  const t0 = performance.now();
-            const { pmcp: pmcp_text } = item;
-            const tokens = tokenize(pmcp_text);
-            for (const tok of tokens) {
-                if (tok.kind === "pmcp-word") {
-                    const query_res = queryLemma(tok.content, true);
-                    if (query_res.kind === "ok") {
-                        highlightable.push(tok.content);
-                    }
-                    else if (isEarthlingWord(tok.content)) {
-                        earthling.push(tok.content);
-                    }
-                    else {
-                        non_highlightable.push(tok.content);
-                    }
+    const countedOccurrences = [...occurrence_map.entries()];
+    const sum = countedOccurrences.reduce((acc, [_k, v]) => acc + v, 0);
+    document.getElementById("total-count").textContent = sum.toString();
+    countedOccurrences.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
+    document.getElementById("output-freq-ranking").value = countedOccurrences.map(([k, v]) => `${v}\t${k}`).join("\n");
+    const highlightable = [];
+    const non_highlightable = [];
+    const earthling = [];
+    const t0 = performance.now();
+    for (const item of corpus_new_to_old) {
+        //  const t0 = performance.now();
+        const { pmcp: pmcp_text } = item;
+        const tokens = tokenize(pmcp_text);
+        for (const tok of tokens) {
+            if (tok.kind === "pmcp-word") {
+                const query_res = queryLemma(tok.content, true);
+                if (query_res.kind === "ok") {
+                    highlightable.push(tok.content);
+                }
+                else if (isEarthlingWord(tok.content)) {
+                    earthling.push(tok.content);
+                }
+                else {
+                    non_highlightable.push(tok.content);
                 }
             }
-            //  const t1 = performance.now();
-            //  console.log(`Inner loop required ${(t1 - t0).toFixed(2)} milliseconds.`);
         }
-        const t1 = performance.now();
-        console.log(`In count_highlightable():\nOuter loop required ${(t1 - t0).toFixed(2)} milliseconds.`);
-        const highlightable_uniq = new Set(highlightable);
-        const non_highlightable_uniq = new Set(non_highlightable);
-        const earthling_uniq = new Set(earthling);
-        const counted = [...non_highlightable.reduce((count, cur) => (count.set(cur, (count.get(cur) || 0) + 1), count), new Map())];
-        counted.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
-        const non_uniq_total = highlightable.length + non_highlightable.length + earthling.length;
-        const uniq_total = highlightable_uniq.size + non_highlightable_uniq.size + earthling_uniq.size;
-        document.getElementById("output-non-highlightable").value = `
+        //  const t1 = performance.now();
+        //  console.log(`Inner loop required ${(t1 - t0).toFixed(2)} milliseconds.`);
+    }
+    const t1 = performance.now();
+    console.log(`In count_highlightable():\nOuter loop required ${(t1 - t0).toFixed(2)} milliseconds.`);
+    const highlightable_uniq = new Set(highlightable);
+    const non_highlightable_uniq = new Set(non_highlightable);
+    const earthling_uniq = new Set(earthling);
+    const counted = [...non_highlightable.reduce((count, cur) => (count.set(cur, (count.get(cur) || 0) + 1), count), new Map())];
+    counted.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
+    const non_uniq_total = highlightable.length + non_highlightable.length + earthling.length;
+    const uniq_total = highlightable_uniq.size + non_highlightable_uniq.size + earthling_uniq.size;
+    document.getElementById("output-non-highlightable").value = `
         highlightable (not uniq): ${highlightable.length}; ${(highlightable.length / non_uniq_total * 100).toPrecision(4)}%
     non-highlightable (not uniq): ${non_highlightable.length}; ${(non_highlightable.length / non_uniq_total * 100).toPrecision(4)}%
         earthling     (not uniq): ${earthling.length}; ${(earthling.length / non_uniq_total * 100).toPrecision(4)}%
@@ -70,7 +67,6 @@ window.gen_stat = function () {
         
     top-tier non-highlightable: ${JSON.stringify(counted)}
     `;
-    }
     document.getElementById("earthling_list").textContent = JSON.stringify(EARTHLING_LIST, null, 2);
     document.getElementById("allowed_sources").innerHTML = "<ul>" + [...EARTHLING_WORDS].map(([source, words]) => `<li>${source}: ${words.join(", ")}</li>`).join("") + "</ul>";
 };
--- ts-src/__corpus_statistics.ts
+++ ts-src/__corpus_statistics.ts
@@ -4,76 +4,73 @@ import { corpus_new_to_old } from "./search.js";
 import { queryLemma } from "./query_lemma.js";
 
 (window as any).gen_stat = function () {
-    {
-        const occurrence_map: Map<string, number> = new Map();
-        for (const item of corpus_new_to_old) {
-            const { pmcp: pmcp_text } = item;
-            const tokens = tokenize(pmcp_text);
-            for (const tok of tokens) {
-                if (tok.kind === "pmcp-word") {
-                    const query_res = queryLemma(tok.content, true);
-                    if (query_res.kind === "ok") {
-                        const lemma = query_res.words.map(w => `(${w.語}|${w.品詞})`).join("");
-                        occurrence_map.set(lemma, (occurrence_map.get(lemma) || 0) + 1);
-                    }
+    const occurrence_map: Map<string, number> = new Map();
+    for (const item of corpus_new_to_old) {
+        const { pmcp: pmcp_text } = item;
+        const tokens = tokenize(pmcp_text);
+        for (const tok of tokens) {
+            if (tok.kind === "pmcp-word") {
+                const query_res = queryLemma(tok.content, true);
+                if (query_res.kind === "ok") {
+                    const lemma = query_res.words.map(w => `(${w.語}|${w.品詞})`).join("");
+                    occurrence_map.set(lemma, (occurrence_map.get(lemma) || 0) + 1);
                 }
             }
         }
-
-        const countedOccurrences: [string, number][] = [...occurrence_map.entries()];
-        const sum = countedOccurrences.reduce((acc, [_k, v]) => acc + v, 0);
-        document.getElementById("total-count")!.textContent = sum.toString();
-        countedOccurrences.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
-
-        (document.getElementById("output-freq-ranking")! as HTMLTextAreaElement).value = countedOccurrences.map(([k, v]) => `${v}\t${k}`).join("\n");
     }
 
-    {
-        const highlightable: string[] = [];
-        const non_highlightable: string[] = [];
-        const earthling: string[] = [];
+    const countedOccurrences: [string, number][] = [...occurrence_map.entries()];
+    const sum = countedOccurrences.reduce((acc, [_k, v]) => acc + v, 0);
+    document.getElementById("total-count")!.textContent = sum.toString();
+    countedOccurrences.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
 
-        const t0 = performance.now();
+    (document.getElementById("output-freq-ranking")! as HTMLTextAreaElement).value = countedOccurrences.map(([k, v]) => `${v}\t${k}`).join("\n");
 
-        for (const item of corpus_new_to_old) {
-            //  const t0 = performance.now();
+    const highlightable: string[] = [];
+    const non_highlightable: string[] = [];
+    const earthling: string[] = [];
 
-            const { pmcp: pmcp_text } = item;
-            const tokens = tokenize(pmcp_text);
-            for (const tok of tokens) {
-                if (tok.kind === "pmcp-word") {
-                    const query_res = queryLemma(tok.content, true);
-                    if (query_res.kind === "ok") {
-                        highlightable.push(tok.content);
-                    } else if (isEarthlingWord(tok.content)) {
-                        earthling.push(tok.content);
-                    } else {
-                        non_highlightable.push(tok.content);
-                    }
+    const t0 = performance.now();
+
+    for (const item of corpus_new_to_old) {
+        //  const t0 = performance.now();
+
+        const { pmcp: pmcp_text } = item;
+        const tokens = tokenize(pmcp_text);
+        for (const tok of tokens) {
+            if (tok.kind === "pmcp-word") {
+                const query_res = queryLemma(tok.content, true);
+                if (query_res.kind === "ok") {
+                    highlightable.push(tok.content);
+                } else if (isEarthlingWord(tok.content)) {
+                    earthling.push(tok.content);
+                } else {
+                    non_highlightable.push(tok.content);
                 }
             }
-            //  const t1 = performance.now();
-            //  console.log(`Inner loop required ${(t1 - t0).toFixed(2)} milliseconds.`);
         }
+        //  const t1 = performance.now();
+        //  console.log(`Inner loop required ${(t1 - t0).toFixed(2)} milliseconds.`);
+    }
 
-        const t1 = performance.now();
-        console.log(`In count_highlightable():\nOuter loop required ${(t1 - t0).toFixed(2)} milliseconds.`);
+    const t1 = performance.now();
+    console.log(`In count_highlightable():\nOuter loop required ${(t1 - t0).toFixed(2)} milliseconds.`);
 
-        const highlightable_uniq = new Set(highlightable);
-        const non_highlightable_uniq = new Set(non_highlightable);
-        const earthling_uniq = new Set(earthling);
+    const highlightable_uniq = new Set(highlightable);
+    const non_highlightable_uniq = new Set(non_highlightable);
+    const earthling_uniq = new Set(earthling);
 
-        const counted: [string, number][] = [...non_highlightable.reduce(
-            (count: Map<string, number>, cur) => (count.set(cur, (count.get(cur) || 0) + 1), count),
-            new Map()
-        )];
+    const counted: [string, number][] = [...non_highlightable.reduce(
+        (count: Map<string, number>, cur) => (count.set(cur, (count.get(cur) || 0) + 1), count),
+        new Map()
+    )];
 
-        counted.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
+    counted.sort(([_k1, v1], [_k2, v2]) => v2 - v1);
 
-        const non_uniq_total = highlightable.length + non_highlightable.length + earthling.length;
-        const uniq_total = highlightable_uniq.size + non_highlightable_uniq.size + earthling_uniq.size;
+    const non_uniq_total = highlightable.length + non_highlightable.length + earthling.length;
+    const uniq_total = highlightable_uniq.size + non_highlightable_uniq.size + earthling_uniq.size;
 
-        (document.getElementById("output-non-highlightable")! as HTMLTextAreaElement).value = `
+    (document.getElementById("output-non-highlightable")! as HTMLTextAreaElement).value = `
         highlightable (not uniq): ${highlightable.length}; ${(highlightable.length / non_uniq_total * 100).toPrecision(4)}%
     non-highlightable (not uniq): ${non_highlightable.length}; ${(non_highlightable.length / non_uniq_total * 100).toPrecision(4)}%
         earthling     (not uniq): ${earthling.length}; ${(earthling.length / non_uniq_total * 100).toPrecision(4)}%
@@ -84,7 +81,7 @@ import { queryLemma } from "./query_lemma.js";
         
     top-tier non-highlightable: ${JSON.stringify(counted)}
     `;
-    }
+
     document.getElementById("earthling_list")!.textContent = JSON.stringify(EARTHLING_LIST, null, 2);
     document.getElementById("allowed_sources")!.innerHTML = "<ul>" + [...EARTHLING_WORDS].map(([source, words]) => `<li>${source}: ${words.join(", ")}</li>`).join("") + "</ul>";
 }
